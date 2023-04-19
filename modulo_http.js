bac8c6b7bd6f94dde94e1f749405c2e230141172
Hebert-Alvarez <hebertalvarez52@hotmail.com>
2023-04-19 11:15:23 -0600

    eckss dee

--- modulo_http.js
+++ modulo_http.js
@@ -1,11 +1,11 @@
 // Llamamos el módulo http
 const http = require('http');
 //Establecemos la url o IP de nuestro servidor
-const hostname = '192.168.1.32';
+const hostname = '169.254.51.153';
 //Establecemos el puerto de escucha
 const port = 3000;
 //Creamos una instancia HTTP con un reques y un response
-const server = https.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     //El servidor responderá un código 200
     res.statusCode = 200;
     //El servidor responderá con un texto plano
@@ -16,4 +16,4 @@ const server = https.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
     console.log('El servidor se está ejecutando en http://${hostname}:${port}/');
-})
+});
